0dca28d3cf2daa9973ee58f31bae24efc96a63cb
keironstoddart <keiron.stoddart@gmail.com>
2023-12-15 14:18:57 -0600

    Clean code

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-"""
-Example public Yahoo league URL: "https://archive.fantasysports.yahoo.com/nfl/2014/729259"
-
-Example vars using public Yahoo leagues still require auth through a personal Yahoo account: see README.md
-
-yfpy Resource: https://github.com/uberfastman/yfpy/blob/main/quickstart/quickstart.pyL
-"""
-
 from flask import Flask, render_template
 from yfpy.query import YahooFantasySportsQuery
 
@@ -13,8 +5,8 @@ from yfpy.query import YahooFantasySportsQuery
 from configurations import CLIENT_ID, CLIENT_SECRET, PATH
 from declarations import Team, RAVINE_RUMBLE, GAME_CODE
 
-
-yahoo_query = YahooFantasySportsQuery(
+# documentation: https://github.com/uberfastman/yfpy
+QUERY_ENGINE = YahooFantasySportsQuery(
     PATH,
     RAVINE_RUMBLE,
     GAME_CODE,
@@ -31,9 +23,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    nan = yahoo_query.get_team_stats_by_week(Team.NAN.value, 15)
-    paul = yahoo_query.get_team_stats_by_week(Team.PAUL.value, 15)
-    tim = yahoo_query.get_team_stats_by_week(Team.TIM.value, 15)
-    tyler = yahoo_query.get_team_stats_by_week(Team.TYLER.value, 15)
+    nan = QUERY_ENGINE.get_team_stats_by_week(Team.NAN.value, 15)
+    paul = QUERY_ENGINE.get_team_stats_by_week(Team.PAUL.value, 15)
+    tim = QUERY_ENGINE.get_team_stats_by_week(Team.TIM.value, 15)
+    tyler = QUERY_ENGINE.get_team_stats_by_week(Team.TYLER.value, 15)
 
     return render_template('losers.html', nan=nan, paul=paul, tim=tim, tyler=tyler)
