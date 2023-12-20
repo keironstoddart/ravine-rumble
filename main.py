5b25fe43bc7e97d0b35c386b73846ad2bb159b42
keironstoddart <keiron.stoddart@gmail.com>
2023-12-19 22:07:17 -0600

    Update for semifinals

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from yfpy.query import YahooFantasySportsQuery
 
 from configurations import CLIENT_ID, CLIENT_SECRET, PATH
 from declarations import Team, RAVINE_RUMBLE, GAME_CODE
-from prediction import predict_winner
 
 
 app = Flask(__name__)
@@ -24,16 +23,12 @@ def home():
         consumer_secret=CLIENT_SECRET,
     )
 
-    nan = query_engine.get_team_stats_by_week(Team.NAN.value, 15)
-    paul = query_engine.get_team_stats_by_week(Team.PAUL.value, 15)
-    tim = query_engine.get_team_stats_by_week(Team.TIM.value, 15)
-    tyler = query_engine.get_team_stats_by_week(Team.TYLER.value, 15)
-
-    nan_prob, paul_prob = predict_winner(nan, paul)
-    tim_prob, tyler_prob = predict_winner(tim, tyler)
+    brett = query_engine.get_team_stats_by_week(Team.BRETT.value, 16)
+    paul = query_engine.get_team_stats_by_week(Team.PAUL.value, 16)
+    wonjoon = query_engine.get_team_stats_by_week(Team.WONJOON.value, 16)
+    tyler = query_engine.get_team_stats_by_week(Team.TYLER.value, 16)
 
     return render_template(
         'losers.html',
-        nan=nan, paul=paul, tim=tim, tyler=tyler,
-        nan_prob=nan_prob, paul_prob=paul_prob, tim_prob=tim_prob, tyler_prob=tyler_prob
+        brett=brett, paul=paul, wonjoon=wonjoon, tyler=tyler,
     )
